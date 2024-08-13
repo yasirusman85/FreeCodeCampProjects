1daaaefcd707997e038318e32e11efd7932b44a4
yasirusman85 <140712050+yasirusman85@users.noreply.github.com>
2024-08-13 15:42:39 +0500

    Add files via upload

---
+++ FreeCodeCamp/FreeCOdcampcourse/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCOdcampcourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name:");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+            Console.Write("Enter you age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(age);
+            Console.WriteLine("Your name is "+ name + " and your age is:"+ age);
+
+
+            //int a=19, b=2;
+            //int remainder = a % b;
+            //Console.WriteLine(remainder);
+
+
+            //string name = "Yasir";
+            //int age = 23;
+            //string number = "012334";
+
+
+            //int i = 0;
+            //Console.WriteLine(i++);
+            //Console.WriteLine(++i);
+            //string name = "Yasir";
+            //name += "Usman";
+            //Console.WriteLine(name);
+
+            //bool value = true;
+            //Console.WriteLine(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //string name = "Yasir";
+            //char a= 'a';
+            //Console.WriteLine(name);
+            //Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+            ////int x = 10,
+            ////    y = 15, 
+            ////    z = 20;
+            //string textage = "32";
+            //int age = Convert.ToInt32(textage);
+            //Console.WriteLine(age);
+            ////Console.WriteLine(int.MaxValue);
+            ////Console.WriteLine(int.MinValue);
+            //string textbignumber = "90000000";
+            //long bigNumber = Convert.ToInt64(textbignumber);
+            //Console.WriteLine(bigNumber);
+            ////Console.WriteLine(long.MaxValue);
+            ////Console.WriteLine(long.MinValue);
+            //string textnegative = "-65.8";
+            //double negative = Convert.ToDouble(textnegative);
+            //Console.WriteLine(negative);
+            ////Console.WriteLine(double.MaxValue);
+            ////Console.WriteLine(double.MinValue);
+            //string textprecision = " 5.000001";
+            //float precision =Convert.ToSingle(textprecision);
+            //Console.WriteLine(precision);
+            ////Console.WriteLine(double.MaxValue);
+            ////Console.WriteLine(double.MinValue);
+            //string textnumber = "14.99";
+            //decimal number = Convert.ToDecimal(textnumber);
+            //Console.WriteLine(number);
+            ////Console.WriteLine(double.MaxValue);
+            ////    Console.WriteLine(double.MinValue);
+
+
+            Console.ReadLine();
+        }
+    }
+}
